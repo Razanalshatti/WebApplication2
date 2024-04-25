5bbe72a608caf231c3c9980a2f5fb810e25313c3
Razan Alshatti <83327@kfh.com>
2024-04-25 11:51:23 +0300

    Add project files.

---
+++ WebApplication2/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(EmployeeData.Employees);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var employee = EmployeeData.Employees.First(x => x.employeeID == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+    }
+}
---
+++ WebApplication2/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication2.Models
+{
+    public class Employee
+    {
+
+        public int employeeID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string department { get; set; }
+    }
+
+        public static class EmployeeData
+        {
+            public static List<Employee> Employees = new List<Employee>
+        {
+            new Employee { employeeID = 1, firstName = "Razan",lastName = "alshatti",email = "R@gmail.com",department = "IT" },
+            new Employee { employeeID = 2, firstName = "Nada",lastName = "alshaibani",email = "N@gmail.com",department = "IT" },
+            new Employee { employeeID = 3, firstName = "Aseel",lastName = "abdulghafour",email = "A@gmail.com",department = "IT"}        };
+        }
+    }
+
